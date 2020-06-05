d8c4a2573637bfb40e7e86a5583a885f2c457ab2
SamFerguson <fergusonsd@g.cofc.edu>
2020-06-05 09:27:37 -0400

    Updates

---
+++ precache-manifest.e443e73435d05dacbaa49e6b55ceb53b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba39a5cf0c1f3403bb11b731b1f8e6a",
+    "url": "/tennis-ladder/index.html"
+  },
+  {
+    "revision": "2367d7f0c0fb2372d019",
+    "url": "/tennis-ladder/static/css/main.ee5f1c4f.chunk.css"
+  },
+  {
+    "revision": "24cbcea4eee67e7f84f1",
+    "url": "/tennis-ladder/static/js/2.1d29ebd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tennis-ladder/static/js/2.1d29ebd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2367d7f0c0fb2372d019",
+    "url": "/tennis-ladder/static/js/main.7d726bf3.chunk.js"
+  },
+  {
+    "revision": "06d05258df5393c3d2c3",
+    "url": "/tennis-ladder/static/js/runtime-main.ee842800.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tennis-ladder/static/media/logo.5d5d9eef.svg"
+  }
+]);
